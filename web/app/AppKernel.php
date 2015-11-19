44a4a5f1e89b417fdcdac24ed74a3c2c665e16a3
Zoran Antolovic <zokadgo@gmail.com>
2015-11-19 23:14:16 +0100

    mandrill

--- web/app/AppKernel.php
+++ web/app/AppKernel.php
@@ -30,6 +30,7 @@ class AppKernel extends Kernel
             new Ivory\CKEditorBundle\IvoryCKEditorBundle(),
             new Knp\Bundle\MenuBundle\KnpMenuBundle(),
             new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
+            new Accord\MandrillSwiftMailerBundle\AccordMandrillSwiftMailerBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
