b79f575a650ac8738f796aa422f453ccbdfc4fbe
Lovro Predovan <lovro.predovan@gmail.com>
2015-11-21 16:21:13 +0100

    structure for randomization

--- web/src/AppBundle/Resources/public/js/main.js
+++ web/src/AppBundle/Resources/public/js/main.js
@@ -54,5 +54,33 @@ $(document).ready(function () {
         connectWith: ".connectedSortable"
     }).disableSelection();
 
+    $('#randomize').on('click',function(){
+        alert('clicked');
+
+
+
+        function shuffle(array) {
+            var currentIndex = array.length, temporaryValue, randomIndex ;
+
+            // While there remain elements to shuffle...
+            while (0 !== currentIndex) {
+
+                // Pick a remaining element...
+                randomIndex = Math.floor(Math.random() * currentIndex);
+                currentIndex -= 1;
+
+                // And swap it with the current element.
+                temporaryValue = array[currentIndex];
+                array[currentIndex] = array[randomIndex];
+                array[randomIndex] = temporaryValue;
+            }
+
+            return array;
+        }
+
+
+    })
+
+
 
 });
