069d6e73ccb6e8c7c36f0cd52e5203c7ee45e1fe
Zoran Antolovic <zokadgo@gmail.com>
2015-11-21 20:42:31 +0100

    test get stats

--- web/tests/api/ApiStatsTest.php
+++ web/tests/api/ApiStatsTest.php
@@ -19,17 +19,36 @@ class ApiStatsTest extends \Codeception\TestCase\Test
     {
     }
 
-    private function checkGenericLoginData()
+    private function checkStatList()
     {
+        $this->tester->seeResponseCodeIs(200);
+        $this->tester->seeResponseIsJson();
+
         $this->tester->seeResponseJsonMatchesJsonPath('$.status');
-        $this->tester->seeResponseJsonMatchesJsonPath('$.data.username');
-        $this->tester->seeResponseJsonMatchesJsonPath('$.data.email');
-        $this->tester->seeResponseJsonMatchesJsonPath('$.data.registrationId');
+        $this->tester->seeResponseJsonMatchesJsonPath('$.data');
+
+        $this->tester->seeResponseJsonMatchesJsonPath('$.data.stats[0].team');
+        $this->tester->seeResponseJsonMatchesJsonPath('$.data.stats[0].player');
+        $this->tester->seeResponseJsonMatchesJsonPath('$.data.stats[0].player.id');
+        $this->tester->seeResponseJsonMatchesJsonPath('$.data.stats[0].player.alias');
+        $this->tester->seeResponseJsonMatchesJsonPath('$.data.stats[0].player.level');
+
+        $this->tester->seeResponseJsonMatchesJsonPath('$.data.stats[0].isLive');
+
+        $this->tester->seeResponseJsonMatchesJsonPath('$.data.stats[0].location');
+        $this->tester->seeResponseJsonMatchesJsonPath('$.data.stats[0].location.lat');
+        $this->tester->seeResponseJsonMatchesJsonPath('$.data.stats[0].location.lng');
+        $this->tester->seeResponseJsonMatchesJsonPath('$.data.stats[0].location.game');
+
     }
 
-    public function testSubmitStat()
+    public function testGetStats()
     {
 
-
+//        $token = $this->doLogin('player1@mailinator.com', '123456');
+//        $this->tester->haveHttpHeader('X-Api-token', $token);
+        $this->tester->haveHttpHeader('Content-type', 'application/json');
+        $this->tester->sendGET('/api/stats/1');
+        $this->checkStatList();
     }
 }
