e91143fd8d4f571be43c1d8a5d7e832d495bcc49
Lovro Predovan <lovro.predovan@gmail.com>
2015-11-21 13:15:32 +0100

    Added basic structure for side navigation

--- web/src/AppBundle/Resources/public/js/main.js
+++ web/src/AppBundle/Resources/public/js/main.js
@@ -1,6 +1,8 @@
 $(document).ready(function () {
 
 
+
+    //sticky footer
     $(window).bind("load", function () {
 
         var footerHeight = 0,
@@ -15,7 +17,7 @@ $(document).ready(function () {
             footerTop = ($(window).scrollTop() + $(window).height() - footerHeight) + "px";
 
             if (($(document.body).height() + footerHeight) < $(window).height()) {
-                $footer.css({position: "absolute"}).animate({top: footerTop},1,'linear').css({opacity: 1})
+                $footer.css({position: "absolute"}).animate({top: footerTop}, 1, 'linear').css({opacity: 1})
             } else {
                 $footer.css({position: "static", opacity: 1})
             }
@@ -25,5 +27,27 @@ $(document).ready(function () {
             .scroll(positionFooter)
             .resize(positionFooter)
     });
+
+    //side navigation
+    $('.navbar-toggle').click(function () {
+        $('.navbar-nav').toggleClass('slide-in');
+        $('.side-body').toggleClass('body-slide-in');
+        $('#search').removeClass('in').addClass('collapse').slideUp(200);
+
+        /// uncomment code for absolute positioning tweek see top comment in css
+        //$('.absolute-wrapper').toggleClass('slide-in');
+
+    });
+
+    // Remove menu for searching
+    $('#search-trigger').click(function () {
+        $('.navbar-nav').removeClass('slide-in');
+        $('.side-body').removeClass('body-slide-in');
+
+        /// uncomment code for absolute positioning tweek see top comment in css
+        //$('.absolute-wrapper').removeClass('slide-in');
+
+    });
+
 
 });
