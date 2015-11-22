b5bcf0f834b80dee39f081f31686cfa00d3d6222
Lovro Predovan <lovro.predovan@gmail.com>
2015-11-22 09:31:53 +0100

    fixing mobile navigation

--- web/src/AppBundle/Resources/public/js/main.js
+++ web/src/AppBundle/Resources/public/js/main.js
@@ -30,22 +30,7 @@ $(document).ready(function () {
         $('.navbar-toggle').click(function () {
             $('.navbar-nav').toggleClass('slide-in');
             $('.side-body').toggleClass('body-slide-in');
-            $('#search').removeClass('in').addClass('collapse').slideUp(200);
-
-            /// uncomment code for absolute positioning tweek see top comment in css
-            //$('.absolute-wrapper').toggleClass('slide-in');
-
         });
-
-        // Remove menu for searching
-        $('#search-trigger').click(function () {
-            $('.navbar-nav').removeClass('slide-in');
-            $('.side-body').removeClass('body-slide-in');
-
-            /// uncomment code for absolute positioning tweek see top comment in css
-            //$('.absolute-wrapper').removeClass('slide-in');
-        });
-
 
         if ($('#home').length > 0) {
 
