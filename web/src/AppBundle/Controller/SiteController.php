862adb415980cedcf4ae326ab751bd680dea6c44
Zoran Antolovic <zokadgo@gmail.com>
2015-11-22 11:14:51 +0100

    profile

--- web/src/AppBundle/Controller/SiteController.php
+++ web/src/AppBundle/Controller/SiteController.php
@@ -230,7 +230,15 @@ class SiteController extends Controller
      */
     public function userProfileAction(Request $request)
     {
-        $player = $this->getDoctrine()->getManager()->getRepository('AppBundle:Player')->findOneBy(['user' => $this->getUser()]);
+
+        $playerId = $request->get('id');
+
+        if (!empty($playerId)) {
+            $player = $this->getDoctrine()->getManager()->getRepository('AppBundle:Player')->find($playerId);
+        } else {
+            $player = $this->getDoctrine()->getManager()->getRepository('AppBundle:Player')->findOneBy(['user' => $this->getUser()]);
+        }
+
         return compact('player');
     }
 
